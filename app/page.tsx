59c47e7749042b672deca3e73ec45158e2c030dd
CSWebmaster <cswebmaster@silveroakuni.ac.in>
2026-09-05 15:54:03 +0530

    feat: position 3D quantum elements starting after event details rail throughout lower sections

--- app/page.tsx
+++ app/page.tsx
@@ -8,20 +8,26 @@ import ScheduleSection from "@/components/schedule/ScheduleSection";
 import ExperienceGrid from "@/components/experience/ExperienceGrid";
 import FAQSection from "@/components/faq/FAQSection";
 import Footer from "@/components/layout/Footer";
+import Background3DElements from "@/components/visuals/Background3DElements";
 
 export default function Home() {
   return (
     <main>
       <Hero />
       <EventInfoRail />
-      <WhyAttend />
-      <QuantumConcepts />
-      <OdysseyTimeline />
-      <SpeakerProfile />
-      <ScheduleSection />
-      <ExperienceGrid />
-      <FAQSection />
-      <Footer />
+
+      {/* 3D background elements start right after the event details rail */}
+      <div style={{ position: "relative", overflow: "hidden" }}>
+        <Background3DElements />
+        <WhyAttend />
+        <QuantumConcepts />
+        <OdysseyTimeline />
+        <SpeakerProfile />
+        <ScheduleSection />
+        <ExperienceGrid />
+        <FAQSection />
+        <Footer />
+      </div>
     </main>
   );
 }
