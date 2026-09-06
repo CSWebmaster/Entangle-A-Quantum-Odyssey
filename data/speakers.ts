85c61cd2081dc56ad780c3d3d788a8ffa30cfa21
CSWebmaster <cswebmaster@silveroakuni.ac.in>
2026-09-06 21:48:13 +0530

    fix: reorder Christian Perez first, fix laser line overlap on countdown, format venue EB-BLOCK on new line, update YouTube link

--- data/speakers.ts
+++ data/speakers.ts
@@ -1,5 +1,13 @@
 export const speakers = [
   {
+    id: "christian-perez",
+    name: "Christian Perez",
+    designation: "Founder & CEO | AWS Community Builder",
+    organization: "Altivum Inc.",
+    image: "/speakers/christian_perez.jpg",
+    linkedin: "https://www.linkedin.com/in/thechrisgrey/",
+  },
+  {
     id: "alok-shukla",
     name: "Dr. Alok Shukla",
     designation: "Assistant Professor & Quantum Computing Researcher",
@@ -7,12 +15,4 @@ export const speakers = [
     image: "/speakers/alok_shukla.jpg",
     linkedin: "https://www.linkedin.com/in/alok-shukla-48930815/",
   },
-  {
-    id: "christian-perez",
-    name: "Christian Perez",
-    designation: "Founder & CEO | AWS Community Builder",
-    organization: "Altivum Inc.",
-    image: "/speakers/christian_perez.jpg",
-    linkedin: "https://www.linkedin.com/in/thechrisgrey/",
-  }
 ];
